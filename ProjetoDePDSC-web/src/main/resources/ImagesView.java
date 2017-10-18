855a57ee6da0a3b7ad6bfcf1dde14cbef756c99c
Tassiors <tassiors@gmail.com>
2017-10-18 12:46:16 -0300

    Definição do template

---
+++ ProjetoDePDSC-web/src/main/resources/ImagesView.java
@@ -0,0 +1,26 @@
+/**
+ *
+ * @author Tássio
+ */
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+ 
+@ManagedBean
+public class ImagesView {
+     
+    private List<String> images;
+     
+    @PostConstruct
+    public void init() {
+        images = new ArrayList<String>();
+        for (int i = 1; i <= 2; i++) {
+            images.add("student" + i + ".jpg");
+        }
+    }
+ 
+    public List<String> getImages() {
+        return images;
+    }
+}
